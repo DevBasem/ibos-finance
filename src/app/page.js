8c2c1f76a4b9cf8941977552c1d8399cec5a5bed
DevBasem <engr.basemahmed@gmail.com>
2024-08-04 19:39:49 +0300

    Welcome Screen Responsive Fix

--- src/app/page.js
+++ src/app/page.js
@@ -4,16 +4,16 @@ import { useRouter } from "next/navigation";
 import Welcome from "./welcome/page";
 
 export default function Home() {
-  const router = useRouter();
+  // const router = useRouter();
 
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      router.push("/login");
-    }, 2000); // 8000 milliseconds = 8 seconds
+  // useEffect(() => {
+  //   const timer = setTimeout(() => {
+  //     router.push("/login");
+  //   }, 2000); // 8000 milliseconds = 8 seconds
 
-    // Cleanup the timeout if the component is unmounted
-    return () => clearTimeout(timer);
-  }, []);
+  //   // Cleanup the timeout if the component is unmounted
+  //   return () => clearTimeout(timer);
+  // }, []);
 
   return <Welcome />;
 }
--- src/app/welcome/page.jsx
+++ src/app/welcome/page.jsx
@@ -10,8 +10,12 @@ export default function Welcome() {
     >
       <main className="grid min-h-screen place-content-center bg-main-gradient text-white">
         <div>
-          <h2 className="text-[32px] font-semibold leading-6">Welcome to</h2>
-          <h1 className="text-[64px] font-extrabold">IBOS Finance</h1>
+          <h2 className="text-[32px] font-semibold leading-6 max-sm:text-[16px] max-sm:leading-[5px]">
+            Welcome to
+          </h2>
+          <h1 className="text-[64px] font-extrabold max-sm:text-[32px]">
+            IBOS Finance
+          </h1>
         </div>
       </main>
     </motion.div>
