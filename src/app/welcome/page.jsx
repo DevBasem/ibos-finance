10e483169c9de65df7f566bfceed498a39d7d36d
DevBasem <engr.basemahmed@gmail.com>
2024-08-04 19:25:58 +0300

    Login & Register

--- src/app/welcome/page.jsx
+++ src/app/welcome/page.jsx
@@ -1,10 +1,19 @@
+"use client";
+import { motion } from "framer-motion";
+
 export default function Welcome() {
   return (
-    <main className="grid min-h-screen place-content-center bg-main-gradient text-white">
-      <div>
-        <h2 className="text-[32px] font-semibold leading-6">Welcome to</h2>
-        <h1 className="text-[64px] font-extrabold">IBOS Finance</h1>
-      </div>
-    </main>
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ ease: "easeInOut", duration: 1.0 }}
+    >
+      <main className="grid min-h-screen place-content-center bg-main-gradient text-white">
+        <div>
+          <h2 className="text-[32px] font-semibold leading-6">Welcome to</h2>
+          <h1 className="text-[64px] font-extrabold">IBOS Finance</h1>
+        </div>
+      </main>
+    </motion.div>
   );
 }
